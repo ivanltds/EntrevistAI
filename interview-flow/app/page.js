208e4abea8353d013472825003bce489776759d2
ivanltds <ivanltds@gmail.com>
2025-10-17 02:53:47 +0000

    criação do projeto Next.js e primeira conexão com a Open AI

---
+++ interview-flow/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Teste API</h1>
+      <a href="/api/test" target="_blank">Abrir rota /api/test</a>
+    </main>
+  );
+}
